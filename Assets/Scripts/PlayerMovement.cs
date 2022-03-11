e2336b327e281b03d1d5977ed89ede917af5250f
512233-JL <512233@chrome.escg.ac.uk>
2022-03-11 01:01:03 +0000

    Added basic movement and animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //Flip player when moving right left
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        anim.SetBool("Walk", horizontalInput != 0);
+
+    }
+}
